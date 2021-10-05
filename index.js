756dd878002a139b82d1a437edb98221e1f5e2c7
Kirstie <kirstiewithrow@gmail.com>
2021-10-05 02:24:27 -0500

    Completed 10.5

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+let destructivelyAppendCat = () => cats.push("Ralph");
+let destructivelyPrependCat = () => cats.unshift("Bob");
+let destructivelyRemoveLastCat = () => cats.pop();
+let destructivelyRemoveFirstCat = () => cats.shift();
+let appendCat = () => [...cats, "Broom"];
+let prependCat = () => ["Arnold", ...cats];
+let removeLastCat = () => cats.slice(0, cats.length - 1);
+let removeFirstCat = () => cats.slice(-2);
